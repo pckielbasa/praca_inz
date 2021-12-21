693c851972067294e68db7c1c7136f04fa409a6b
pckielbasa <pckielbasa@gmail.com>
2021-12-21 15:25:49 +0100

    Changing medicine on contact

---
+++ app/src/main/java/com/example/praca_inz/ui/calendar/day/DayFragment.kt
@@ -0,0 +1,19 @@
+package com.example.praca_inz.ui.calendar.day
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.praca_inz.R
+
+class DayFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_day, container, false)
+    }
+
+}
